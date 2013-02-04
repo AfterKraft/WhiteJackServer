c6751e265fa6201020f806fd03b1336024cb7566
Gabriel Harris-Rouquette <gabizou@me.com>
2013-02-03 23:30:08 -0800

    Added some Server stuff. Doesn't work, but just need to commit before doing other things.

--- src/main/java/com/whitejack/net/server/GameLobby.java
+++ src/main/java/com/whitejack/net/server/GameLobby.java
@@ -1,7 +1,44 @@
 package com.whitejack.net.server;
 
-public class GameLobby {
-	
-	
+import java.util.HashMap;
+import java.util.List;
 
+import com.whitejack.api.Game;
+import com.whitejack.api.GameFactory;
+import com.whitejack.api.GameMaker;
+import com.whitejack.api.GameTable;
+import com.whitejack.api.User;
+
+public class GameLobby<G extends Game, GT extends GameTable<G>, GF extends GameFactory<G, GT>> {
+	
+	protected GameMaker<G, GT, GF> gameMaker;
+	private int numOfTables;
+	private HashMap<GameTable<G>, List<User>> mapOfTables;
+	
+	public void setup() {
+		
+		for(int i=0; i>=numOfTables; i++){
+			gameMaker = new GameMaker<G, GT, GF>(4);
+			mapOfTables.put(gameMaker.getGameTable(), gameMaker.getGameTable().getUsers());
+		}
+
+		
+	}
+	
+	/**
+	 * Sets the maximum number of Tables allowed in this game lobby
+	 * instance
+	 */
+	public void setNumOfTables(int num) {
+		this.numOfTables = num;
+	}
+
+	/**
+	 * Returns the set number of Tables allowed in this game lobby
+	 * instance
+	 * @return
+	 */
+	public int getNumOfTables() {
+		return numOfTables;
+	}
 }
--- src/main/java/com/whitejack/net/server/WhiteServer.java
+++ src/main/java/com/whitejack/net/server/WhiteServer.java
@@ -1,23 +1,112 @@
 package com.whitejack.net.server;
 
+import java.awt.BorderLayout;
+//import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
+import com.whitejack.api.Game;
+import com.whitejack.api.GameFactory;
+import com.whitejack.api.GameTable;
 import com.whitejack.api.net.server.Server;
 
+public class WhiteServer<G extends Game, GT extends GameTable<G>, GF extends GameFactory<G, GT>> extends Server {
 
-public class WhiteServer extends Server {
-	
-	private final boolean onlineMode = true;
+//	private final boolean onlineMode = true;
 	private static final Logger log = Logger.getLogger("WhiteJack");
-	
+	private JTextArea jta = new JTextArea();
+	private GameLobby<G, GT, GF> lobby;
+
 	private static Server server;
-	
+
 	public static void main(String[] args) {
 		BasicConfigurator.configure();
 		log.info("Welcome to WhiteJack Server!");
-		server = new WhiteServer();
-		log.info("Starting up server version "+ server.getVersion());
+		server = new WhiteServer<Game, GameTable<Game>, GameFactory<Game, GameTable<Game>>>();
+		log.info("Starting up server version " + server.getVersion());
+	}
+
+	/**
+	 * Constructs a server instance with multithreading for multiple clients.
+	 */
+	private WhiteServer() {
+		JFrame frame = new JFrame();
+		frame.setLayout(new BorderLayout());
+		frame.add(new JScrollPane(jta), BorderLayout.CENTER);
+		frame.setTitle("WhiteJack Server v" + super.getVersion());
+		frame.setSize(500, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+		try {
+			// Make a server socket!
+			ServerSocket serverSocket = new ServerSocket(5170);
+			jta.append("WhiteJack Server Started at" + new Date() + '\n');
+
+			lobby.setNumOfTables(5);
+			lobby.setup();
+			int clientNo = 1;
+			while (true) {
+				Socket socket = serverSocket.accept();
+
+				jta.append("Started Thread for client " + clientNo + "at "
+						+ new Date() + "\n");
+
+				InetAddress inetAddress = socket.getInetAddress();
+				jta.append("Client " + clientNo + "'s host name is "
+						+ inetAddress.getHostName() + "\n");
+				jta.append("Client " + clientNo + "'s IP address is "
+						+ inetAddress.getHostAddress() + "\n");
+
+				HandleClient task = new HandleClient(socket);
+
+				new Thread(task).start();
+
+				clientNo++;
+			}
+			
+		} catch (IOException ex) {
+			log.warn(ex);
+		} finally {
+			
+		}
+	}
+
+	class HandleClient implements Runnable {
+
+		private Socket socket;
+
+		public HandleClient(Socket socket) {
+			this.socket = socket;
+		}
+
+		@Override
+		public void run() {
+			try {
+//				DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
+				DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
+
+				while (true) {
+					// Do stuff here
+					outputToClient.writeChars("You've connected to WhiteJack Server v" + getVersion());
+				}
+			} catch (IOException ex) {
+				log.warn(ex);
+			}
+
+		}
+
 	}
 
 }
